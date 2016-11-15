12408778145d5b07662bd267a4ed2dc6fc848e38
wimke-b <wimke_b@hotmail.com>
2016-11-15 22:48:44 +0100

    Day One Development
    HTML - Created the App Content
    CSS - Styled Content
    Javascript - Added functionality

---
+++ www/js/app.js
@@ -0,0 +1,33 @@
+//Global Variable
+var taskList = [];
+
+//Display Dynamic HTML Lists
+//UL & LI Tags
+//Remove Button Displays
+function displayList()
+{
+    
+    //Variable for creating and storing dynamic HTML Elements
+    var taskListContent = "<ul>";
+    
+    //Create LI Tags and Loop through the Array
+    for(var i=0; i < taskList.length; i++)
+    {
+        taskListContent += "<li>"+taskList[i]+"<button>"
+        document.
+    }
+}
+
+function add ()
+{
+    
+}
+function remove ()
+{
+    
+}
+function  display ()
+{
+    
+}
+
